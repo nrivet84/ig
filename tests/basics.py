c1b19af7229d582f7bd474a05a679cf45e3c9bf8
nrivet84 <nicolasrivet84@live.fr>
2016-12-27 20:15:50 +0100

    add proxy + fix import modules

--- tests/basics.py
+++ tests/basics.py
@@ -5,8 +5,12 @@ test the connection to IG API
 do some basic operations
 """
 
-from ig.ig_service import IGservice as igs
-import ig.ig_tools as igt
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'ig')))
+
+import ig_service as igs
+import ig_tools as igt
 
 def main():
     """Main module for testing."""
@@ -16,20 +20,20 @@ def main():
     igt.getconfig('demo')
     
     #login demo API
-    service=igs(username, password, api_key, account, 'demo')
-    log=igs.login(service)
-    print(log[0])
+    service=igs.IGservice(username, password, api_key, account, 'demo', proxy_user, proxy_password)
+    log=service.login()
+    print('\n', 'login', '\n', log)
     
     #get newest bidask
     instrument='CS.D.EURUSD.CFD.IP'
-    bidask=igs.get_bidask(service, instrument)
-    print(bidask)
+    bidask=service.get_bidask(instrument)
+    print('\n', 'get_bidask of EURUSD', '\n', bidask)
     
     #get historical closes
     resolution='MINUTE'
     max_size=10
-    closes=igs.get_closes(service, instrument, resolution, max_size)
-    print(closes)
+    closes=service.get_closes(instrument, resolution, max_size)
+    print('\n', 'get_closes of EURUSD for the last 10 minutes', '\n', closes)
     
 if __name__ == '__main__':
     main()
