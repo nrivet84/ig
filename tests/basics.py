f8aa863247636b2b1440394f839710d39a8f360d
nrivet84 <nicolasrivet84@live.fr>
2016-12-27 20:30:46 +0100

    manage proxy + fix import modules + PEP8

--- tests/basics.py
+++ tests/basics.py
@@ -14,26 +14,28 @@ import ig_tools as igt
 
 def main():
     """Main module for testing."""
-    
+
     #get config for demo API
     proxy_user, proxy_password, api_key, username, password, account = \
     igt.getconfig('demo')
-    
+
     #login demo API
-    service=igs.IGservice(username, password, api_key, account, 'demo', proxy_user, proxy_password)
-    log=service.login()
+    service = igs.IGservice(username, password, api_key, account, 'demo',\
+                            proxy_user, proxy_password)
+    log = service.login()
     print('\n', 'login', '\n', log)
-    
+
     #get newest bidask
-    instrument='CS.D.EURUSD.CFD.IP'
-    bidask=service.get_bidask(instrument)
+    instrument = 'CS.D.EURUSD.CFD.IP'
+    bidask = service.get_bidask(instrument)
     print('\n', 'get_bidask of EURUSD', '\n', bidask)
-    
+
     #get historical closes
-    resolution='MINUTE'
-    max_size=10
-    closes=service.get_closes(instrument, resolution, max_size)
+    resolution = 'MINUTE'
+    max_size = 10
+    closes = service.get_closes(instrument, resolution, max_size)
     print('\n', 'get_closes of EURUSD for the last 10 minutes', '\n', closes)
-    
+
 if __name__ == '__main__':
     main()
+    
